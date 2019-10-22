23cb9b13facf3adfec2f7c49d4fa5ca80c03c19f
muratvuranok <muratvuranok@hotmail.com>
2019-10-22 12:19:05 +0300

    Ekran hazırlandı

--- Operatorler/Form1.Designer.cs
+++ Operatorler/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnToplama = new System.Windows.Forms.Button();
+            this.btnCikartma = new System.Windows.Forms.Button();
+            this.btnCarpma = new System.Windows.Forms.Button();
+            this.btnBolme = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnToplama
+            // 
+            this.btnToplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToplama.Location = new System.Drawing.Point(13, 22);
+            this.btnToplama.Name = "btnToplama";
+            this.btnToplama.Size = new System.Drawing.Size(406, 66);
+            this.btnToplama.TabIndex = 0;
+            this.btnToplama.Text = "Toplama";
+            this.btnToplama.UseVisualStyleBackColor = true;
+            // 
+            // btnCikartma
+            // 
+            this.btnCikartma.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCikartma.Location = new System.Drawing.Point(13, 94);
+            this.btnCikartma.Name = "btnCikartma";
+            this.btnCikartma.Size = new System.Drawing.Size(406, 66);
+            this.btnCikartma.TabIndex = 0;
+            this.btnCikartma.Text = "Çıkartma";
+            this.btnCikartma.UseVisualStyleBackColor = true;
+            // 
+            // btnCarpma
+            // 
+            this.btnCarpma.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarpma.Location = new System.Drawing.Point(13, 166);
+            this.btnCarpma.Name = "btnCarpma";
+            this.btnCarpma.Size = new System.Drawing.Size(406, 66);
+            this.btnCarpma.TabIndex = 0;
+            this.btnCarpma.Text = "Çarpma";
+            this.btnCarpma.UseVisualStyleBackColor = true;
+            // 
+            // btnBolme
+            // 
+            this.btnBolme.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBolme.Location = new System.Drawing.Point(13, 238);
+            this.btnBolme.Name = "btnBolme";
+            this.btnBolme.Size = new System.Drawing.Size(406, 66);
+            this.btnBolme.TabIndex = 0;
+            this.btnBolme.Text = "Bölme";
+            this.btnBolme.UseVisualStyleBackColor = true;
+            // 
+            // btnMod
+            // 
+            this.btnMod.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMod.Location = new System.Drawing.Point(13, 310);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(406, 66);
+            this.btnMod.TabIndex = 0;
+            this.btnMod.Text = "Mod";
+            this.btnMod.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(431, 450);
+            this.ClientSize = new System.Drawing.Size(431, 395);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.btnBolme);
+            this.Controls.Add(this.btnCarpma);
+            this.Controls.Add(this.btnCikartma);
+            this.Controls.Add(this.btnToplama);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +102,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnToplama;
+        private System.Windows.Forms.Button btnCikartma;
+        private System.Windows.Forms.Button btnCarpma;
+        private System.Windows.Forms.Button btnBolme;
+        private System.Windows.Forms.Button btnMod;
     }
 }
 
